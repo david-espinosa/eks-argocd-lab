a0327884fcff462f09397b74da1fce37de5e24cc
David Espinosa <espinosa.eduard@gmail.com>
2026-06-20 11:53:49 +0200

    feat: add S3 backend with native locking

--- versions.tf
+++ versions.tf
@@ -1,10 +1,17 @@
 terraform {
-  required_version = ">= 1.8.5"
+  required_version = ">= 1.15.6"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 6.0"
     }
+  }
+
+  backend "s3" {
+    bucket = "eks-espi-lab-tfstate"
+    key = "eks-espi-lab/terraform.tfstate"
+    region = "eu-north-1"
+    use_lockfile = "true"
   }
 }
